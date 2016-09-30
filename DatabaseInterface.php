f43c5725de452f26e24eb093f0bfa2c0898722e2
ConnorWhite <connor.bcw@gmail.com>
2016-09-30 14:14:13 -0500

    TODO table of contents

--- DatabaseInterface.php
+++ DatabaseInterface.php
@@ -7,3 +7,7 @@
   function addPlayer($name, $pass){
     addRow("Player", array("Name", "Password"), array($name, $pass));
   }
+  //TODO: Return all Courts within $range of ($lat, $lon)
+  function getCourts($latitude, $longitude, $range){
+    
+  }
