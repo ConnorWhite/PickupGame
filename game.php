a2e7fcbc63dbee3ffe0dd75819e06d5dbbb133b2
Ziping Liu <ziping@utexas.edu>
2016-10-21 18:47:30 -0500

    initial commit for game.php implementation

--- game.php
+++ game.php
@@ -1,6 +1,36 @@
 <?php
-	include 'pattern/Observer.php';
 
+	session_start();
+	//include 'pattern/Observer.php';
+	include 'DatabaseAdapter.php';
+
+	// Set in gameID
+	//$gameID = $_SESSION['gameID'];
+	$gameviewID = 2;
+	$gameview = getGameById($gameviewID);
+	$courtLabel = "Court  | ";
+	$dateLabel  = "Date   | ";
+	$playerLabel = "Current Player List:";
+	$players = getPlayers($gameviewID);
+	?>
+
+	<!--Display the Game's court and date, then all the players in that game -->
+	<div class="displayGame">
+		  <h1 class="game-title"><a><?php echo $gameview["Name"]; ?></a></h2><br />
+			<?php $court = getCourtByID($gameview["CourtID"]); ?>
+			<h2 class="court"><a><?php echo$courtLabel;echo$court["Name"];?></a>
+			<h2 class="date"><a><?php echo$dateLabel;echo$gameview["Date"];?></a>
+			<h2 class="players"><a><?php echo$playerLabel;?></a>
+				<?php foreach($players as $player){ ?>
+					<h3 class="player"><a><?php echo $player["Name"];?></a>
+				<?php } ?>
+			</h2><br />			</h2><br />
+
+	</div>
+
+	<?php include 'chat.php'
+
+/*
 	class Game implements Subject {
 		private $id = -1; // uninitialized
 		private $name;
@@ -14,7 +44,7 @@
 			$name = $_name;
 			$court = $_court;
 			$time = $_time;
-		}		
+		}
 		public function register(Observer player) {
 			$players[$player->id] = $player;
 		}
@@ -29,4 +59,4 @@
 			}
 		}
 
-	}
+	} */ ?>
