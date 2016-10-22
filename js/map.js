e671d12e34d8d6f2a30f24f7619c85b8e4b0cbe7
Ziping Liu <ziping@utexas.edu>
2016-10-21 21:18:45 -0500

    add court button - initial commit

--- js/map.js
+++ js/map.js
@@ -1,4 +1,10 @@
 var map;
+// In the following example, markers appear when the user clicks on the map.
+// Each marker is labeled with a single alphabetical character.
+var labels = '+';
+var labelIndex = 0;
+var marker;
+
 function initMap() {
   //Create map
   mapDiv = document.getElementById('map');
@@ -13,4 +19,32 @@ function initMap() {
   //Set header position to fixed
   document.getElementById("header").style.position = "fixed";
   //TODO: remove 'Map' from menu
+
+  // Ability to place marker for a new court's location
+  google.maps.event.addListener(map, 'click', function(event) {
+    placeMarker(event.latLng);
+  });
+}
+
+
+function addCourt(marker) {
+//  marker.location;
+
+  if(confirm("Adding New Court"))
+  {
+  }
+
+}
+function placeMarker(location) {
+  if ( marker ) {
+    marker.setPosition(location);
+    marker.visible = true;
+  } else {
+    marker = new google.maps.Marker({
+      position: location,
+      label: "+",
+      map: map
+    });
+    marker.addListener('click', function(){addCourt(marker);});
+  }
 }
