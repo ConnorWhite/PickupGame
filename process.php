080e0eef873952f01690abcc3e3f6d2dcf86d586
ronny <ronnymacmaster@gmail.com>
2016-10-26 16:41:36 -0500

    process clean

--- process.php
+++ process.php
@@ -8,17 +8,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     
     switch($function) {
     
-    	 case('getState'):
-        	 if(file_exists("$id.txt")){
-               $lines = file("$id.txt");
+    	 case('getChatState'):
+		 	 $chatlog = "chat/logs/$id.txt";
+        	 if(file_exists($chatlog)){
+               $lines = file($chatlog);
         	 }
              $log['state'] = count($lines); 
         	 break;	
     	
-    	 case('update'):
+    	 case('updateChat'):
+		 	$chatlog = "chat/logs/$id.txt";
         	$state = $_POST['state'];
-        	if(file_exists("$id.txt")){
-        	   $lines = file("$id.txt");
+        	if(file_exists("$chatlog")){
+        	   $lines = file("$chatlog");
         	 }
         	 $count =  count($lines);
         	 if($state == $count){
@@ -41,23 +43,18 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         	  
              break;
     	 
-    	 case('send'):
-		  $nickname = htmlentities(strip_tags($_POST['nickname']));
-			 $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
-			  $message = htmlentities(strip_tags($_POST['message']));
-		 if(($message) != "\n"){
-        	
-			 if(preg_match($reg_exUrl, $message, $url)) {
-       			$message = preg_replace($reg_exUrl, '<a href="'.$url[0].'" target="_blank">'.$url[0].'</a>', $message);
-				} 
-			 
-        	
-        	 fwrite(fopen("$id.txt", 'a'), "<span>". $nickname . "</span>" . $message = str_replace("\n", " ", $message) . "\n"); 
-		 }
-        	 break;
-    	
-    }
-    
-    echo json_encode($log);
-}
+    	 case('sendChat'):
+			$chatlog = "chat/logs/$id.txt";
+			$nickname = htmlentities(strip_tags($_POST['nickname']));
+			$reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+			$message = htmlentities(strip_tags($_POST['message']));
+			if(($message) != "\n"){
+				if(preg_match($reg_exUrl, $message, $url)) {
+       				$message = preg_replace($reg_exUrl, '<a href="'.$url[0].'" target="_blank">'.$url[0].'</a>', $message);
+					fwrite(fopen("$chatlog", 'a'), "<span>". $nickname . "</span>" . $message = str_replace("\n", " ", $message) . "\n"); 
+				}
+			}
+			break;
+	}
+	echo json_encode($log);
 ?>
