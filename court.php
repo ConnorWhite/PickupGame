3032f045085991ecfbae94eb3979c424a0f32ed1
ginsengtang <ginsengtang@bitbucket.org>
2016-11-04 15:32:52 -0500

    added some stuff to court

--- court.php
+++ court.php
@@ -1,5 +1,20 @@
 <?php
-  //TODO
-
   session_start();
-  $CourtID = $_SESSION["CourtID"];
+  $title = "Gregory Gym Court"; //TODO change to a session variable
+  include 'header.php';
+  
+ 
+  $stack = array("orange", "banana");
+  array_push($stack, "apple", "raspberry");
+  print_r($stack);
+
+?>
+
+<div style="color:#0000FF">
+  <h3>This is a heading</h3>
+  <p>This is a paragraph.</p>
+</div>
+
+<?php
+  include 'footer.php';
+?>
