ae4d40180c8ba620e5ebbaf7d54e1eceaf9c50e8
ginsengtang <gtang.gt@gmail.com>
2016-11-06 16:29:34 -0600

    getting games for a particular court

--- court.php
+++ court.php
@@ -1,18 +1,20 @@
 <?php
   include 'DatabaseFacade.php';
   session_start();
-  $CourtID = $_SESSION["CourtID"];
-  $court = getCourtByID($CourtID);
-  $title =  "Games for " + $court['Name'];
+  $_SESSION['CourtID'] = 1;
+  $court = getCourtByID($_SESSION["CourtID"]);
+  $title =  "Games for " . $court['Name'];
+  
+  var_dump(getGamesByCourtID($_SESSION['CourtID']));
+  
   include 'header.php';
 //comment
-?>
 
-<div style="color:#0000FF">
-  <h3>This is a heading</h3>
-  <p>This is a paragraph.</p>
-</div>
-
-<?php
+for ($x = 0; $x <= 6; $x++) {
+    echo "<div class= \"game\">
+	<h3>Game " . $x ."</h3><p>This is a paragraph.</p>
+	</div>";
+	}
+	
   include 'footer.php';
 ?>
