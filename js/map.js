fccf42c6f8b88db498fe7d7428511c6921c4bdff
root <ziping@utexas.edu>
2016-11-18 16:59:11 -0600

    fixed map.js some more

--- js/map.js
+++ js/map.js
@@ -117,7 +117,28 @@ function addCourt(marker) {
   {
     // display new court pin on map
     //var courtName = prompt("Please enter this new court's name", "Court Name");
-    var submit_flag = $("#addCourtDialog").dialog("open");
+
+    var courtNameIn = '<input id = "CourtName" type="text" name="FirstName" value="Court Name"><br>';
+    var courtSubmit = '<button id = SubmitCourt>Submit</button>';
+    infowindow = new google.maps.InfoWindow({
+      content: "Enter Court Name: " + courtNameIn +
+      courtSubmit
+    });
+
+    infowindow.open(map, marker);
+
+
+    $("#SubmitCourt")[0].addEventListener("click", function(){
+      var courtData = [];
+      courtData["Name"] = $('#CourtName').val();
+      console.log("infow window add court: " + courtData['Name']);
+      courtData["Latitude"] = addCourtMarker.position.lat();
+      courtData["Longitude"] = addCourtMarker.position.lng();
+      addCourtLevelDialog(true, courtData);
+      infowindow.close();
+      marker.visible = false;});
+
+
 
   } else
       alert("Add Court Canceled");
@@ -376,7 +397,7 @@ games_titles = games_titles.concat("<p>There are " + courtInfo[i]["PlayerData"].
 + " Total Player(s) for " + courtInfo[i]["GameData"]["Name"] + ":<br>");
 for(var j = 0; j < courtInfo[i]["PlayerData"].length; j++)
 {
-  if(courtInfo[i]["PlayerData"][j] != null) // hack
+  if(courtInfo[i]["PlayerData"][j] != null)
 games_titles = games_titles.concat(courtInfo[i]["PlayerData"][j]["Name"].concat("<br>"));
 }
 }
@@ -400,7 +421,7 @@ $("#courtButt")[0].addEventListener("click", function(){takeUserToTheRequestedCo
     $( "#courtInfoDialog" ).dialog({
 
       dialogClass : "jquery_form",
-      title : "Court | ".concat(courtData['Name']),/
+      title : "Court | ".concat(courtData['Name']),
       autoOpen: false,
       width: 400,
       buttons: [
