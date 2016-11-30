2f30d523b26181d976734d78c312081db5e29728
ConnorWhite <connor.bcw@gmail.com>
2016-11-29 19:15:35 -0600

    Hopefully fixed header issue in court.php

--- court.php
+++ court.php
@@ -1,35 +1,28 @@
 <?php
   include 'DatabaseFacade.php';
   session_start();
-  //$_SESSION['playerID'] = 2; // set in login.php
-  $court = null;
-  if(isset($_GET["courtID"]))
-  {
-    $court = getCourtByID($_GET["courtID"]); // http set in mygames.php
+  if(isset($_POST['addgame'])) {
+    header('Location: addgame.php?courtID='.$_GET["courtID"]);
   }
-  else
-  {
+  $court = null;
+  if(isset($_GET["courtID"])){
+    $court = getCourtByID($_GET["courtID"]); // http set in mygames.php
+  } else {
     $court = getCourtByID($_SESSION["CourtID"]); // session set in map.js
   }
 
   $title =  $court['Name'];
-
   $games = getGamesByCourtID($court['ID']);
 
-  //var_dump($games);
-  include 'header.php'; ?>
+  include 'header.php';
+?>
   <div class="wrap">
-
-  <?php
-    foreach ($games as $game) {
-      include 'gameButton.php';
-    }?>
-
+    <?php foreach ($games as $game) {
+        include 'gameButton.php';
+    } ?>
     <form id="addgame" method="post" action="">
       <input type="submit" name="addgame" value="Add New Game"/>
     </form>
   </div>
-  <?php if(isset($_POST['addgame'])) {
-    header('Location: addgame.php?courtID='.$_GET["courtID"]);
-  }
+<?php
   include 'footer.php';
